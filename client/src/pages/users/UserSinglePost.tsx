3c87a1a0a0bed2438fbbe42b799c63b4ab855c86
Mary <marieshix98@gmail.com>
2023-03-08 15:49:25 +0300

    Add single users post route

--- client/src/pages/users/UserSinglePost.tsx
+++ client/src/pages/users/UserSinglePost.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useGetAllPostsCommentsQuery } from '../../features/user/commentApi'
 
 const UserSinglePost = () => {
+  const {id} = useParams()
+  const {data} = useGetAllPostsCommentsQuery(id)
+  console.log(data,'post comments')
+ 
   return (
     <div>UserSinglePost</div>
   )
--- client/src/routes/Allroutes.tsx
+++ client/src/routes/Allroutes.tsx
@@ -6,6 +6,7 @@ const NotFound = React.lazy(() => import("../pages/error/NotFound"));
 const Home = React.lazy(() => import("../pages/Home"));
 const SinglePost = React.lazy(() => import("../pages/posts/SinglePost"));
 const Posts = React.lazy(() => import("../pages/users/Posts"));
+const UserSinglePost = React.lazy(() =>import("../pages/users/UserSinglePost"))
 
 const Allroutes = () => {
   return (
@@ -15,6 +16,7 @@ const Allroutes = () => {
       <Route path="/posts" element={<AllPosts />} />
       <Route path="/userPosts" element={<Posts />} />
       <Route path="/singlePost/:id" element={<SinglePost />} />
+      <Route path="/userPost/:id" element={<UserSinglePost/>}/>
       <Route path="*" element={<NotFound/>}/>
     </Routes>
   );
